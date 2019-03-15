2e203cecc7cf7d3cc844541a53ecb40a0cf09dce
Yaroslav Erohin <ysoftware@users.noreply.github.com>
2019-03-15 15:23:14 +0300

    simple campaign full run test

--- tests/methods.py
+++ tests/methods.py
@@ -3,7 +3,7 @@ from eosfactory.eosf import *
 from datetime import datetime
 import time
 
-investors = ["investora", "investorb", "investorc", "investord", "investore"]
+investors = ["investora", "investorb", "investorc", "investord", "investore", "investorf", "investorg"]
 
 # methods
 
@@ -32,41 +32,90 @@ def transfer(contract, fromAccount, to, quantity, memo):
 		},
 		permission=[(fromAccount, Permission.ACTIVE)])
 
-def newcampaign(eosioscrugex):
+def newcampaign(eosioscrugex, founder):
 	timestamp = int(time.time()*1000.0)
 	SECOND = 1000
 
 	eosioscrugex.push_action("newcampaign",
 		{
-			"campaignId": 0,
-			"founderUserId": 0, 
+			"supplyForSale": "100.0000 TEST",
+			"tokenContract": "token",
 			"founderEosAccount": "founder",
-			"softCap": "250000.0000 EOS",
-			"hardCap": "500000.0000 EOS",
-			"initialFundsReleasePercent": 490000,
-			"minUserContribution": "10.0000 EOS",
-			"maxUserContribution": "200000.0000 EOS",
-			"publicTokenPercent": 800000,
-			"tokenSupply": 1000000000,
-			"startTimestamp": timestamp,
-			"endTimestamp": timestamp + 10 * SECOND,
-			"annualInflationPercentStart": 0,
-			"annualInflationPercentEnd": 0,
+			"softCap": "10.0000 EOS",
+			"hardCap": "20.0000 EOS",
+			"initialFundsReleasePercent": 25,
+			"kycEnabled": False,
+			"minUserContribution": "0.0010 EOS",
+			"maxUserContributionPercent": 35,
+			"publicTokenPercent": 80,
+			"startTimestamp": timestamp + 1000,
+			"campaignDuration": 8 * SECOND,
 			"milestones": [
 				{
-					"deadline": timestamp + 20 * SECOND,
-					"fundsReleasePercent": 260000
+					"duration": 2 * SECOND,
+					"fundsReleasePercent": 25
 				},
 				{
-					"deadline": timestamp + 40 * SECOND,
-					"fundsReleasePercent": 250000
+					"duration": 2 * SECOND,
+					"fundsReleasePercent": 25
+				},
+				{
+					"duration": 2 * SECOND,
+					"fundsReleasePercent": 25
 				}
 			]
 		},
+		permission=[(founder, Permission.ACTIVE)])
+
+
+def vote(eosioscrugex, account, vote):
+	eosioscrugex.push_action("vote", 
+		{
+			"eosAccount": account,
+			"campaignId": 0,
+			"vote": vote
+		},
+		permission=[(account, Permission.ACTIVE)])
+
+def send(eosioscrugex, account):
+	eosioscrugex.push_action("send", 
+		{
+			"eosAccount": account,
+			"campaignId": 0
+		},
 		permission=[(eosioscrugex, Permission.ACTIVE)])
 
+def sell(eosioscrugex, account, quantity):
+	eosioscrugex.push_action("sell", 
+		{
+			"eosAccount": account,
+			"campaignId": 0,
+			"quantity": quantity
+		},
+		permission=[(account, Permission.ACTIVE)])
+
+def take(eosioscrugex, account):
+	eosioscrugex.push_action("take", 
+		{
+			"eosAccount": account,
+			"campaignId": 0
+		},
+		permission=[(account, Permission.ACTIVE)])
+
+def buy(eosioscrugex, account, quantity, summ):
+	eosioscrugex.push_action("buy", 
+		{
+			"eosAccount": account,
+			"campaignId": 0,
+			"quantity": quantity,
+			"sum": summ
+		},
+		permission=[(account, Permission.ACTIVE)])
 
 def refresh(eosioscrugex):
 	eosioscrugex.push_action("refresh", 
 		"[]",
 		permission=[(eosioscrugex, Permission.ACTIVE)])
+
+def balance(token, account):
+	return float(token.table("accounts", account).json["rows"][0]["balance"].split(" ")[0])
---
+++ tests/simple.py
@@ -0,0 +1,156 @@
+import unittest
+from eosfactory.eosf import *
+from methods import *
+from time import sleep
+import string
+
+verbosity([Verbosity.INFO, Verbosity.OUT, Verbosity.TRACE, Verbosity.DEBUG])
+
+CONTRACT_WORKSPACE = "ScrugeX"
+
+# methods
+
+class Test(unittest.TestCase):
+
+	# setup
+
+	@classmethod
+	def tearDownClass(cls):
+		stop()
+
+	@classmethod
+	def setUpClass(cls):
+		SCENARIO("Test newproject action")
+		reset()
+
+		create_master_account("master")
+		create_account("founder", master, "founder")
+
+		key = CreateKey(is_verbose=False)
+
+		# Token
+
+		create_account("eosio_token", master, "eosio.token")
+		create_account("token", master, "token")
+
+		token_contract = Contract(eosio_token, "02_eosio_token")
+		if not token_contract.is_built():
+			token_contract.build()
+		token_contract.deploy()
+
+		token_contract = Contract(token, "02_eosio_token")
+		if not token_contract.is_built():
+			token_contract.build()
+		token_contract.deploy()
+
+		# ScrugeX
+
+		create_account("eosioscrugex", master, "eosioscrugex", key)
+		eosioscrugex.set_account_permission(
+			Permission.ACTIVE,
+			{
+					"threshold" : 1,
+					"keys" : [{ "key": key.key_public, "weight": 1 }],
+					"accounts": [{
+						"permission": {
+							"actor": "eosioscrugex",
+							"permission": "eosio.code"
+						},
+						"weight": 1
+					}],
+				},
+			Permission.OWNER, (eosioscrugex, Permission.OWNER))
+
+		bounty_contract = Contract(eosioscrugex, "ScrugeX/src")
+		if not bounty_contract.is_built():
+			bounty_contract.build()
+		bounty_contract.deploy()
+
+		# Distribute tokens
+
+		create_issue(eosio_token, master, "EOS")
+		create_issue(token, founder, "TEST")
+
+		# Users
+
+		for s in investors:
+			create_account(s, master, s)
+			transfer(eosio_token, master, s, "1000000.0000 EOS", "")
+
+	def run(self, result=None):
+		super().run(result)
+
+	# tests
+
+	def test_create_project(self):
+		newcampaign(eosioscrugex, founder)
+		transfer(token, founder, eosioscrugex, "100.0000 TEST", "0")
+
+		sleep(2)
+
+		for s in investors:
+			transfer(eosio_token, s, eosioscrugex, "2.5000 EOS", "0")
+
+		sleep(5)
+		refresh(eosioscrugex)
+
+		sleep(5)
+		refresh(eosioscrugex)
+
+		for s in investors:
+			vote(eosioscrugex, s, True)
+
+		refresh(eosioscrugex)
+
+		sleep(3)
+		refresh(eosioscrugex)
+		
+		sleep(5)
+		refresh(eosioscrugex)
+
+		for i in range(0, 3):
+			sleep(2)
+			refresh(eosioscrugex)
+
+		# MILESTONE 2
+
+		for s in investors:
+			vote(eosioscrugex, s, True)
+
+		sleep(5)
+		refresh(eosioscrugex)
+
+		# MILESTONE 3
+
+		for s in investors:
+			vote(eosioscrugex, s, True)
+
+		refresh(eosioscrugex)
+		
+		sleep(5)
+		refresh(eosioscrugex)
+
+		# read updates
+
+		sleep(20)
+		refresh(eosioscrugex)
+
+		sleep(1)
+		eosioscrugex.table("information", eosioscrugex)
+		eosioscrugex.table("campaigns", eosioscrugex)
+		eosioscrugex.table("contribution", "0")
+		eosioscrugex.table("voting", "0")
+
+		# check leftover tokens	
+		self.assertLess(balance(eosio_token, eosioscrugex), 0.01)
+		self.assertLess(balance(token, eosioscrugex), 0.001)
+
+		# check tokens received
+		self.assertGreater(balance(token, investora), 14)
+
+
+
+# main
+
+if __name__ == "__main__":
+	unittest.main()
